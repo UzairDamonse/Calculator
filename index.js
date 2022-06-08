0a40421e7d05f8a4340c8152f97f3ae6166a5c74
UzairDamonse <uzairdamonse03@gmail.com>
2022-06-08 11:58:12 +0200

    added color changing buttons

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ function Reset() {
   document.getElementById(`Output`).innerHTML = "0";
   sum = "";
 }
+
+// text color changers
+function red() {
+  document.getElementById(`Output`).style.color = "rgb(188, 12, 12)";
+}
+function green() {
+  document.getElementById(`Output`).style.color = "rgb(61, 210, 28)";
+}
+function blue() {
+  document.getElementById(`Output`).style.color = "rgba(92, 92, 208, 0.662)";
+}
